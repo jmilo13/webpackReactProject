0a66321cf2e0d5909a58e2b9f3fd115447a1b065
Camilo <camilogm3@gmail.com>
2021-04-10 20:46:42 -0300

    inicio repositorio

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => <div class="container">
+        <h1>👽</h1>
+        <h1 class="title">Primeras lineas en React con Webpack</h1>
+        <p class="paragraph">Un paso más en el recorrido</p>
+        <p> 🥳  🥳  🥳 </p>
+    </div>
+
+export default App
